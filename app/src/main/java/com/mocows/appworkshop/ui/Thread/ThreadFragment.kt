c61109e460c9f7611b2fecf49cab4561f8a0f41c
aomari99 <52887341+aomari99@users.noreply.github.com>
2021-01-26 01:01:46 +0100

    add myfiles

---
+++ app/src/main/java/com/mocows/appworkshop/ui/Thread/ThreadFragment.kt
@@ -0,0 +1,25 @@
+package com.mocows.appworkshop.ui.Thread
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.mocows.appworkshop.R
+
+class ThreadFragment : Fragment() {
+ 
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_thread, container, false)
+
+        return root
+    }
+}
